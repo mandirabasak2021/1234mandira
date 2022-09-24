9a391e2f142b9dd0896ff286894100a7bc19ff78
Mandira basak <109147959+mandirabasak2021@users.noreply.github.com>
2022-09-24 13:28:38 +0530

    Create Q1.c

---
+++ Assignment 4/Q1.c
@@ -0,0 +1,11 @@
+//Write a program to print MySirG 5 times on the screen
+#include <stdio.h>
+
+int main(void){
+      int a;
+      for(a=0;a<5;a++)
+      {
+        printf("MySirG\n");
+      }
+      return 0;
+}
